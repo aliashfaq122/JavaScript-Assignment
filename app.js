38152b082be982efaab0efc0883384b4a378d8cf
Ali Ashfaq <muhammedaliashfaq@gmail.com>
2026-02-20 16:20:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,138 @@
+// CHP#1
+
+// Q1
+//  alert ("Welcome to our website")
+
+// Q2
+// alert ("Error! Please enter a valid password.")
+
+// Q3
+// alert ("Welcome to Our Server... \nEnjoy Shopping.")
+
+// Q4
+// alert ("Welcome to Js Land.")
+// alert ("Enjoy coding. \n\n\nClick ok to close this alert")
+
+// Q5
+// var alert = "Hello... I can run JS through my website browser's console"
+// console.log ("alert")
+
+// CHP#2
+
+// Q1
+// let username ="Your Name?"
+
+// Q2
+// let myname ="Ali"
+
+// Q3
+// var message;
+// message="Hello World!"
+// alert(message);
+
+// Q4
+// let Name="Jhone Doe"
+// alert (Name);
+
+// let age="15 years old"
+// alert (age);
+
+// let skill="Modern Web Development"
+// alert (skill);
+
+// Q5
+// var food="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert (food);
+
+// Q6
+// var text="My Email Address is"
+// var Email="muhammedaliashfaq@gmail.com"
+// alert (text+" "+ Email)
+
+// Q7
+// var book="A smarter\nway to learn JavaScript"
+// alert ("I am trying to learn from a Book "+ " " + book)
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Q9
+// let design = " \n▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert (design);
+// document.write (design);
+
+// CHP#3
+
+// Q1
+// var age="I am 16 years old";
+// alert (age)
+
+// Q2
+// var numberOfVisits="12"
+// alert ("You have visited this site"+ " " + numberOfVisits + " " + "times.");
+
+//  Q3
+// var birthyear = "My birth year is 2012"
+// document.write(birthyear)
+
+// Q4
+// var user="John Doe"
+// var orders="T-Shirts"
+// var quantity="5"
+// document.write(user+" "+"ordered"+" "+quantity+" "+orders+" "+"from Daraz.")
+
+// CHP#4
+
+// Q1
+// var name="Ali", Age="16", city="Karachi"
+
+// Q2
+// legal name
+
+// var name=""
+// var _myuser=""
+// var city=""
+// var $price=""
+// var usernames=""
+
+// illegal name
+
+// var 2-user=""
+// var user-3=""
+// var var=""
+// var console=""
+// var @user=""
+
+// Q3
+// document.write("Rules for naming JS variables \n");
+// document.write(" 1-Variable names can only contain letters, numbers, $ and _ \n");
+// document.write("2-Variables must begin with a letter, $ or _ \n");
+// document.write("3-Variable names are case sensitive.");
+// document.write("4-Variable names should not be JS reserved keywords.");
+
+// CHP#5
+
+// Q1
+// var num1 = 3, num2 = 5
+// var totalNum = num1 + num2
+// alert("Sum of" + " " + num1 + " " + "and" + " " + num2 + " " + "is" + " " + totalNum)
+
+// Q2
+// subtraction
+
+// var num= 5-9
+// document.write( "The sum of 5 - 9 is " + num )
+// Divide
+
+// var num2 = 5 * 9
+// document.write( "The sum of 5*9 is " + num )
+// percentage
+
+// var num3 = 5 % 9
+// document.write( "The sum of 5%9 is " + num )
+
+// Q3
+// var num4 = 5 + 1
+// var add = num4 + 7
+// document.write(add + " is the value")
