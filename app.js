35f13df64360f660156563a439d784a03ac4a5a7
Ali Ashfaq <muhammedaliashfaq@gmail.com>
2026-02-24 16:55:54 +0500

    assignment updated

--- app.js
+++ app.js
@@ -136,3 +136,120 @@
 // var num4 = 5 + 1
 // var add = num4 + 7
 // document.write(add + " is the value")
+
+// Q4
+// var tktprice = 600;
+// console.log ("the total price of 6 tickets are" + " " + tktprice * 6)
+
+// Q5
+// console.log(4*1);
+// console.log(4*2);
+// console.log(4*3);
+// console.log(4*4);
+// console.log(4*5);
+
+// Q7
+// var item1price = 650
+// var item2price = 100
+// var item1quantity = 3
+// var item2quantity = 7
+// var shippingcharges = 100
+// console.log("the total cost of your order is" + " " + (item1quantity * item1price + item2quantity * item2price + shippingcharges));
+
+// Q8
+// var totalmarks = 980
+// var obtmarks = 804
+// console.log("Percentage:"+804/980*100);
+
+// Q9
+// var dollar = 104.80
+// var riyal = 28
+// console.log("Total currency in pkr is" + " " + (dollar*10+riyal*25));
+
+// Q10
+// var num = 6
+// console.log(num+5*10/2);
+
+// Q11
+// var birthyear = 1971
+// var currentyear = 2026
+// var age = currentyear - birthyear
+// console.log("Your current Age is" + " " + age);
+
+// Q12
+// var fvtsnack = "chips"
+// var currentage = 16
+// var maximumage = 110
+// var remainingage = maximumage-currentage
+// var amountofsnackperday = 2
+// console.log("You will have" + " " + amountofsnackperday*remainingage +" " +fvtsnack+" "+"in your life");
+
+// CHP#6-8
+
+// Q1
+// var a = 10
+// console.log("value of ++a" + " " +"is" + " " + ++a);
+// console.log("value of ++a" + " " +"is" + " " + ++a);
+// console.log("value of --a" + " " +"is" + " " + --a);
+// console.log("value of a--" + " " +"is" + " " + a--);
+// console.log("Now the value of a-- is" + " " + a  );
+
+// Q2
+// var a = 2, b = 1;
+// console.log(--a);
+// console.log(--a - --b);
+// console.log(--a - --b + ++b);
+// console.log(--a - --b + ++b + b--);
+
+// Q3
+// var user = prompt ("Enter your name", "Ali")
+// alert ("Hello" + " " + user)
+
+// Q5
+// var Subject1 = prompt ("Enter your first subject name", "English")
+// var Subject2 = prompt ("Enter your second subject name", "Maths")
+// var Subject3 = prompt ("Enter your third subject name", "Urdu")
+// var totalMarks = 1100
+// var Subject1marks = prompt ("Enter your" + " " + Subject1 + " " + "marks")
+// var Subject2marks = prompt ("Enter your" + " " + Subject2 + " " + "marks")
+// var Subject3marks = prompt ("Enter your" + " " + Subject3 + " " + "marks")
+// console.log("You obtained" + " " + (Subject1marks+Subject2marks+Subject3marks/totalMarks));
+
+// CHP#9
+// Q1
+// var city = prompt ("Enter your city");
+// if (city === "karachi") {
+//     alert ("Welcome to city of lights")   
+// }
+
+// Q2
+// var gender = prompt("Enter your gender")
+// if (gender === "Male") {
+//     alert("welcome sir")
+// }
+
+// if (gender === "Female") {
+//     alert("welcome mam")
+// }
+
+// Q3
+// var traffic = prompt("Enter color of traffic signal")
+// if (traffic === "Red") {
+//     alert("Must stop")
+// }
+
+// if (traffic === "Yellow") {
+//     alert("Ready to move")
+// }
+
+// if (traffic === "Green") {
+//     alert("Move now")
+// }
+
+// Q4
+var remainingfuel = prompt ("Please enter how much litres of fuel is remaining?")
+if (remainingfuel <= 0.25) {
+    alert ("Please refill your tank")
+}else {
+    alert ("You can continue your journey")
+}
